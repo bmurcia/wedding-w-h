aa69674f51b81a3f5fd32516e70fed7426b92e2d
bmurcia <bnmurci4@gmail.com>
2024-11-16 11:28:41 -0600

    Se agrego la seccion de fotos

---
+++ src/hooks/TextPhotos.ts
@@ -0,0 +1,36 @@
+export const events = [
+    {
+        text: "Coincidimos en que fuimos bautizados en la misma fecha",
+        image:"/pagePhotos_1.jpg",
+        alt:"Foto de Bautizmo"
+    },
+    {
+        text: "El día que William declaró sus amor . 10 jun 2023 💗",
+        image:"/pagePhotos_2.jpg",
+        alt:"Foto de William y Heidy"
+    },
+    {
+        text: "Primer viaje de novios",
+        image:"/pagePhotos_3.jpg",
+        alt:"Foto de William y Heidy en el parque"
+    },
+    {
+        text: "Después de un culto en nuestra iglesia 💒",
+        image:"/pagePhotos_4.jpg",
+        alt:"Foto de William y Heidy en la iglesia"
+    },
+    {
+        text: "A ambos nos gusta estar entre arboles disfrutando la naturaleza de Dios  pero aún más si estamos juntos ♥️",
+        image:"/pagePhotos_5.jpg",
+        alt:"Foto de William y Heidy en la naturaleza"
+    },
+    {
+        text: "Celebramos nuestro aniversario juntos ♥️22.06.2023- 22.06.2024♥️",
+        image:"/pagePhotos_6.jpg",
+        alt:"Foto de William y Heidy en la naturaleza"
+    }
+    
+
+]
+
+
