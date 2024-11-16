f163a844ce5b054a22646dc2ddcff113980e4954
bmurcia <bnmurci4@gmail.com>
2024-11-15 19:43:06 -0600

    Se agrego pagina OurStory

---
+++ src/hooks/weddingDate.ts
@@ -0,0 +1,7 @@
+export const title = "William & Heidy";
+export const title2 = "W&H"
+export const date = "December 7, 2024";
+export const date2 = "07.12.2024"
+export const place = " Choloma";
+export const city = 'Honduras'
+export const hour = "5:00 pm"
